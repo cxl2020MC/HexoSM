e34a2e60260ca6f5c7a954b2088ff93bfc736835
cxl2020MC <andamu2019@163.com>
2022-10-31 21:01:40 +0800

    vscode

--- web/api/__init__.py
+++ web/api/__init__.py
@@ -1,19 +1,10 @@
+from crypt import methods
 from flask import Blueprint, request, jsonify, session, redirect, render_template
 import os
 
 bp = Blueprint('api', __name__)
 
-@bp.route('/api/<apiname>/')
-def index(apiname):
-    data = request.json
-    print(data)
-    if apiname == "xxx":
-        retdata = login(data)
-    else:
-        return jsonify({"ok": False, "msg": "404错误: 在请求的服务器上找不到请求的URL", "data": None})
-    return jsonify({"ok": True, "msg": "ok", "data": retdata})
-
-@bp.route('/api/login/')
+@bp.route('/api/login/', methods = ["POST"])
 def login(data):
     data = request.json
     print(data)
@@ -27,3 +18,13 @@ def login(data):
         login = True
         session["login"] = True
     return {"ok": True, "msg": msg, "login": login}
+
+@bp.route('/api/<apiname>/', methods = ["POST"])
+def index(apiname):
+    data = request.json
+    print(data)
+    if apiname == "xxx":
+        retdata = login(data)
+    else:
+        return jsonify({"ok": False, "msg": "404错误: 在请求的服务器上找不到请求的URL", "data": None})
+    return jsonify({"ok": True, "msg": "ok", "data": retdata})
